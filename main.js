0388464c05d66447bb6a3f8968785fb40d86f337
DuloeW <duloekeren@gmail.com>
2022-07-06 20:10:40 +0800

    Frst Commit

---
+++ main.js
@@ -0,0 +1,8 @@
+const humberOn = document.getElementById('burgerOn');
+const humberOf = document.getElementById('burgerOf');
+let menu = document.getElementById('menu');
+
+humberOn.addEventListener('click', function() {
+  menu.style.display = 'block';
+})
+
